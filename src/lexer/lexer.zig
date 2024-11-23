c2935cad6ed9d5db5d797429c666e4705f63e470
Munkherdene <muunuu960@gmail.com>
2024-11-23 21:15:41 +0800

    init

---
+++ src/lexer/lexer.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+const Token = enum {
+    Let,
+};
+
+pub fn lex(expr: []const u8) !Token {
+    const stdout = std.io.getStdOut().writer();
+    _ = try stdout.write(expr);
+    std.debug.print(@tagName(Token.Let), .{});
+    return Token.Let;
+}

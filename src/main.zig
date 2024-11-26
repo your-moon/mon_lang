6ad1edb4365f179bd784dd09f32942717be29b4d
Munkherdene <muunuu960@gmail.com>
2024-11-26 16:48:16 +0800

    fmt

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,6 @@ const parser = @import("parser/parser.zig");
 const lexer = @import("lexer/lexer.zig");
 
 pub fn main() !void {
-    std.debug.print("Main", .{});
-    parser.parse();
+    _ = try parser.parse("hello this is source");
     _ = try lexer.lex("hi");
 }
--- src/parser/parser.zig
+++ src/parser/parser.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
-pub fn parse() void {
-    std.debug.print("Its parser", .{});
+pub fn parse(source: []const u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{s}\n", .{source});
 }
